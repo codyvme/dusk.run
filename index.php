dee74e2dc0f318740cecb9c8d872d6b9feb885ea
Cody VerKuilen <clverkuilen@gmail.com>
2024-10-07 14:48:42 -0400

    Parking my domain here for the time being until I figure out what to do with it

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>🌆</title>
+        <style>
+            html {
+                height: 100%;
+            }
+            
+            body {
+                background: url(images/dusk.jpg) no-repeat center center/cover;
+            }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>
